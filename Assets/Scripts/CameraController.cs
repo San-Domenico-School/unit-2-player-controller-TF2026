aedba3a8b16e3a5942f2cf6c9559e7136acd5c68
TF2026 <tfleitas26@sandomenico.org>
2023-09-12 13:53:09 -0700

    2.7

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/***************************
+ * Component of the Main Camera that keeps it
+ * a constant distance from the vehicle
+ * 
+ * Teddy Fleitas
+ * September 11, 2023 Version 17.6
+ **************************/
+
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform vehicleTransform;
-
-    private Vector3 offset;
-
+    [Tooltip("Drag Vehicle onto Vehicle Transform")]
+    [SerializeField] private Transform vehicleTransform;  // to keep track of vehicle's position
+    private Vector3 offset;                               // distance camera is from vehicle
 
     // Start is called before the first frame update
     void Start()
